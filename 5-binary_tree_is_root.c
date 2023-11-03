18b65da04436ff472a0ba5c9510aa8dfeb366533
Sark <kosinick01@gmail.com>
2023-11-03 22:44:57 +0100

    5. Is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -12,7 +12,6 @@ int binary_tree_is_root(const binary_tree_t *node)
 	if (!node)
 		return (0);
 	if (node->parent == NULL)
-		if (node->left || node->right)
-			return (1);
+		return (1);
 	return (0);
 }
